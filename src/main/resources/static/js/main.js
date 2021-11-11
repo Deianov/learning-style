71f587766eeb29d2b12cbdfd88e8f47971256728
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2021-11-11 23:07:28 +0100

    gradle - merge and minify css

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,7 +3,7 @@ import {router} from "./modules/factory.js";
 
 
 // default loader
-await router.navigate(0, null, router.urlSearchParams);
+router.navigate(0, null, router.urlSearchParams);
 
 // footer
 document.getElementById("cdate").innerHTML = (new Date().getFullYear());
