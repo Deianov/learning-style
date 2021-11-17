2640c85ee857e32e6db5506212be7f3bd3bf4657
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2021-11-17 17:16:52 +0100

    refactoring the js, gradle for Merge and Minify

--- src/main/resources/static/js/modules/utils/dom.js
+++ src/main/resources/static/js/modules/utils/dom.js
@@ -1,38 +1,74 @@
-function element(name, parent, options) {
-    const e = document.createElement(name)
-    parent.appendChild(e)
-    if (options) {setOptions(e, options)}
-    return e
-}
-
-function node(text, parent) {
-    const node = document.createTextNode(text)
-    parent.appendChild(node)
-    return node
-}
-
-function text(name, parent, text, options) {
-    const e = element(name, parent)
-    e.textContent = text;
-    if (options) {setOptions(e, options)}
-    return e
-}
-
-function removeAll(parent) {
-    if (parent) {
-        parent.innerHTML = "";
-    }
-    // while (parent.firstChild) {
-    //     parent.removeChild(parent.firstChild);
-    // }
-}
-
-function remove(e) {
-    if (e) {
-        const parent = e.parentNode;
-        if (parent) {
-            parent.removeChild(e)
+const dom = {
+    /**
+     * @param {string | HTMLElement} v   -  id | element
+     * @returns {HTMLElement}
+     */
+    get(v) {
+        return (typeof v === "string") ? document.getElementById(v) : v
+    },
+    /**
+     * @param {HTMLElement} e       - element
+     * @param {string | object} o   - className or object with options (key, value).
+     */
+    setOptions (e, o) {
+        if (typeof o === "string") {
+            e.setAttribute("class", o);
+            return
         }
+        for (const [k, v] of Object.entries(o)) {
+            if (k === "class" || k === "key") {
+                e.setAttribute(k, v);
+            } else {
+                e[k] = v
+            }
+        }
+    },
+    /**
+     * @param {string} n            - name
+     * @param {HTMLElement} p       - parent
+     * @param {string | object} o   - className or object with options (key, value).
+     * @returns {HTMLElement}       - element
+     */
+    element(n, p, o) {
+        const e = document.createElement(n)
+        p.appendChild(e)
+        if (o) {this.setOptions(e, o)}
+        return e
+    },
+    /**
+     * @param {string} t            - text
+     * @param {HTMLElement} p       - parent
+     * @returns {Text}
+     */
+    node(t, p) {
+        const node = document.createTextNode(t)
+        p.appendChild(node)
+        return node
+    },
+    /**
+     * @param {string} n            - name
+     * @param {HTMLElement} p       - parent
+     * @param {string} t            - text
+     * @param {string | object} o   - className or object with options (key, value).
+     * @returns {HTMLElement}
+     */
+    text(n, p, t, o) {
+        const e = this.element(n, p)
+        e.textContent = t;
+        if (o) {this.setOptions(e, o)}
+        return e
+    },
+    /**
+     * @param {HTMLElement} p       - parent
+     */
+    removeAll(p) {
+        if (p) { p.innerHTML = "" }
+    },
+    /**
+     * @param {HTMLElement} e       - element
+     */
+    remove(e) {
+        return e ? e.parentNode ? e.parentNode.removeChild(e) : 0 : 0
     }
 }
 
@@ -42,39 +78,18 @@ function updateText(id, textOrNode) {
     e.textContent = ""; // Removes all children
     e.append(textOrNode);
 }
-
 function update(id, callback, arg) {
     const e = document.getElementById(id);
     if (e && callback) {
         callback(e, arg)
     }
 }
+function removeAll(parent) {
+    while (parent.firstChild) {
+         parent.removeChild(parent.firstChild);
+    }
+}
 */
 
-/**
- * @param {string | HTMLElement} value id | element
- */
-function get(value) {
-    return (typeof value === "string") ? document.getElementById(value) : value
-}
-
-/**
- * @param {HTMLElement} e 
- * @param {string | object} options String (className) or object with options.
- */
-function setOptions (e, options) {
-    if (typeof options === "string") {
-        e.setAttribute("class", options);
-        return
-    }
-    for (const [key, value] of Object.entries(options)) {
-        if (key === "class" || key === "key") {
-            e.setAttribute(key, value);
-        } else {
-            e[key] = value
-        }
-    }
-}
-
-const dom = {element, text, node, removeAll, remove, get};
+// const dom = {element, text, node, removeAll, remove, get};
 export default dom;
