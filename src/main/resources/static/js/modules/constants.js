9c58ffa63e5fe4bf90b1a15a230f7c8e1923893f
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2021-12-03 18:31:17 +0100

    development state - css colors

--- src/main/resources/static/js/modules/constants.js
+++ src/main/resources/static/js/modules/constants.js
@@ -22,6 +22,41 @@ constants_system.infoString+= (constants_system.isMobile ? "mobile OS: " : "OS: 
 constants_system.infoString+= navigator.userAgent;
 constants_system.infoString+= (constants_system.isDarkMode ? ", DarkMode" : "");
 document.getElementsByClassName("version")[0].textContent = constants_system.infoString;
+const CSS_COLORS = [
+    {name: "white", colors: ["white"]},
+    {name: "black", colors: ["black"]},
+    {name: "dark", colors: ["#373737"]},
+    {name: "green", colors: ["#28a745"]},
+    {name: "red", colors: ["#dc3545"]},
+    {name: "orange", colors: ["#ff6600"]},
+    {name: "-", colors: [""]},
+    {name: "gray-dark", colors: ["#333333"]},
+    {name: "gray", colors: ["#646464"]},
+    {name: "gray-light", colors: ["#959595"]},
+    {name: "gray-text", colors: ["#cacaca"]},
+    {name: "gray-line", colors: ["#dedede"]},
+    {name: "--", colors: [""]},
+    {name: "light-red", colors: ["#feaec8"]},
+    {name: "light-blue", colors: ["#99d9e9"]},
+    {name: "light-yellow", colors: ["#efe4b0"]},
+    {name: "light-pink", colors: ["#c7bfe6"]},
+    {name: "---", colors: [""]},
+    {name: "links", colors: ["#0275d8"]},
+    {name: "color-bg", colors: ["#f1f1f1"]},
+    {name: "color-bg-hover", colors: ["#ddd"]},
+    {name: "hover", colors: ["#1795ec"]},
+    {name: "active", colors: ["#4299E1"]},
+    {name: "border", colors: ["#ccc"]},
+    {name: "submit", colors: ["#4caf50"]},
+    {name: "submit-hover", colors: ["#45a049"]},
+    {name: "logo", colors: ["#517cc6"]},
+    {name: "----", colors: [""]},
+    {name: "success", colors: ["#c1ffde", "#c1ffde", "#dbfff8"]},
+    {name: "danger", colors: ["#ffcede"]},
+    {name: "error", colors: ["#e00"]},
+    {name: "info", colors: ["#4299E1"]},
+    {name: "alert-success", colors: ["#00A4A6"]},
+];
 
 
 const constants_server = {
@@ -54,6 +89,7 @@ ms.en.input = {}
 ms.en.bnt = {}
 ms.en.quiz = {}
 ms.en.keyboard = {}
+ms.en.server = {}
 
 ms.de = {}
 ms.de.input = {}
@@ -79,15 +115,18 @@ ms.en.keyboard.byString = "keyboard: virtual (auto)";
 ms.en.quiz.server = "Server validation is not supported on static version.";
 ms.en.quiz.corect = "Not found correct answers.";
 
+ms.en.server.required = "Server side required!";
 
 const CS = {
     app: constants_app,
     system: constants_system,
     server: constants_server,
     dom: constants_dom,
-    get msg() {
-        return ms[constants_app.lang];
-    }
+    colors: CSS_COLORS,
+    // get msg() {
+    //     return ms[constants_app.lang];
+    // }
+    msg: ms.en,
 }
 
 export default CS;
