9581e860acdded01d688a150584ece766c96866d
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2021-12-05 16:29:51 +0100

    refactoring

--- src/main/java/bg/geist/web/interceptors/UserInterceptor.java
+++ src/main/java/bg/geist/web/interceptors/UserInterceptor.java
@@ -1,5 +1,9 @@
 package bg.geist.web.interceptors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import bg.geist.constant.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,33 +13,96 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.SmartView;
+import org.springframework.web.servlet.View;
 
 
 @Component
 public class UserInterceptor implements HandlerInterceptor {
-    private static final Logger logger = LoggerFactory.getLogger(UserInterceptor.class);
+    private static final Logger log = LoggerFactory.getLogger(UserInterceptor.class);
 
-
+    /**
+     * Executed before actual handler is executed
+     **/
     @Override
-    public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler, final ModelAndView modelAndView) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+        if (isUserLogged()) {
+            addToModelUserDetails(request.getSession());
+        }
+        return true;
+    }
 
-        if (modelAndView != null && isAuthenticated()) {
-            String viewName = modelAndView.getViewName();
-            if (viewName == null || viewName.startsWith("redirect:/")) {
+    /**
+     * Executed before after handler is executed. If view is a redirect view, we don't need to execute postHandle
+     **/
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object, ModelAndView model) throws Exception {
+
+        if (!isRedirectView(model) && isUserLogged()) {
+            addToModelUserDetails(model);
+        }
+
+        // don't change !
+        if (!isRedirectView(model) && isAuthenticated()) {
+            if (model.getModel().containsKey(Constants.PROFILE_KEY)) {
                 return;
             }
-            if (modelAndView.getModel().containsKey(Constants.PROFILE_KEY)) {
-                return;
-            }
-            modelAndView.addObject(Constants.PROFILE_KEY, request.getSession().getAttribute(Constants.PROFILE_KEY));
-            logger.info("add profile attributes to view: " + viewName);
+            model.addObject(Constants.PROFILE_KEY, request.getSession().getAttribute(Constants.PROFILE_KEY));
+            log.info("add profile attributes to view: " + model.getViewName());
         }
     }
 
-    private boolean isAuthenticated() {
+    /**
+     * Used before model is generated, based on session
+     */
+    private void addToModelUserDetails(HttpSession session) {
+        log.info("================= addToModelUserDetails ============================");
+        String loggedUsername = SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getName();
+        session.setAttribute("username", loggedUsername);
+        log.info("user(" + loggedUsername + ") session : " + session);
+        log.info("================= addToModelUserDetails ============================");
+
+    }
+
+    /**
+     * Used when model is available
+     */
+    private void addToModelUserDetails(ModelAndView model) {
+        log.info("================= addToModelUserDetails ============================");
+        String loggedUsername = SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getName();
+        model.addObject("loggedUsername", loggedUsername);
+        log.trace("session : " + model.getModel());
+        log.info("================= addToModelUserDetails ============================");
+
+    }
+
+    private static boolean isRedirectView(ModelAndView mv) {
+        if (mv == null || !mv.hasView() || mv.getViewName() == null) {
+            return false;
+        }
+        if (mv.getViewName().startsWith("redirect:/")) {
+            return true;
+        }
+        View view = mv.getView();
+        return (view instanceof SmartView && ((SmartView) view).isRedirectView());
+    }
+
+    private static boolean isUserLogged() {
+        try {
+            return !SecurityContextHolder.getContext()
+                    .getAuthentication()
+                    .getName()
+                    .equals("anonymousUser");
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static boolean isAuthenticated() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass())) {
             return false;
