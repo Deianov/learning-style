19e6ca501031038d5551364733e581a89ee075bf
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2021-12-05 16:31:50 +0100

    interceptor not used

--- src/main/java/bg/geist/config/WebConfig.java
+++ src/main/java/bg/geist/config/WebConfig.java
@@ -28,6 +28,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(userInterceptor);
-        registry.addInterceptor(faviconInterceptor);
+//        registry.addInterceptor(faviconInterceptor);
     }
 }
