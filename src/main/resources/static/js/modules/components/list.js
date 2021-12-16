c564a0ad3dba43f5dcb9a60406c571da9410c332
Ognian Deianov <44973718+Deianov@users.noreply.github.com>
2021-12-16 18:39:18 +0100

    bug fixed

--- src/main/resources/static/js/modules/components/list.js
+++ src/main/resources/static/js/modules/components/list.js
@@ -18,11 +18,17 @@ class List extends Component {
         this.json = jsonFile;
         this.cols = this.json.state.tabs.length;
         this.opt = options;
-        List.renderRows(this.element, this.cols ,this.json.data, this.opt);
         /** editable */
         if (this.opt && this.opt.contenteditable) {
+            this.numbering = true;
+            this.cols++;
             dom.setOptions(this.element, {spellcheck: "false", autocapitalize: "none", autocomplete: "off"});
+            this.element.classList.toggle("numbering", true);
+        } else {
+            this.element.classList.toggle("numbering", false);
+            this.numbering = undefined;
         }
+        List.renderRows(this.element, this.cols ,this.json.data, this.numbering, this.opt);
     }
     /** @param {number|null} column - update column|columns visibility from state */
     update(column) {
@@ -49,34 +55,42 @@ class List extends Component {
         return true;
     }
     read() {
-        if (this.element) {
-            const rows = this.element.rows.length;
-            /** constant length - matrix ! */
-            const cols = this.element.rows[0].cells.length;
-            const arr = [];
-            const empty = [];
-            for (let r = 0; r < rows; r++) {
-                const a = Array(cols);
-                const cells = this.element.rows[r].cells;
-                /** skip rows with empty cell */
-                if (this.isFilled(cells)) {
-                    for (let c = 0; c < cols; c++) {
-                        a[c] = cells[c].textContent || "";
-                    }
-                    /** removing duplicates */
-                    if (!arr.some(ar => ar.every((v, i) => v === a[i]))) {
-                        arr.push(a);
-                    }
-                } else {
-                    empty.push(this.element.rows[r]);
-                }
-            }
-            empty.forEach(tr => dom.remove(tr));
-            return arr;
+        if (!this.element || !this.element.rows.length) {
+            return [];
         }
+        const rows = this.element.rows.length;
+        /** constant length - matrix ! */
+        const cols = this.element.rows[0].cells.length;
+        const arr = [];
+        const empty = [];
+        for (let r = 0; r < rows; r++) {
+            const a = Array(cols);
+            const cells = this.element.rows[r].cells;
+            /** skip rows with empty cell */
+            if (this.isFilled(cells)) {
+                for (let c = 0; c < cols; c++) {
+                    a[c] = cells[c].textContent || "";
+                }
+                /** removing duplicates */
+                if (!arr.some(ar => List.equals(ar, a, this.numbering))) {
+                    arr.push(a);
+                }
+            } else {
+                empty.push(this.element.rows[r]);
+            }
+        }
+        empty.forEach(tr => dom.remove(tr));
+        return arr;
     }
     add() {
-        List.renderRow(this.element, this.cols, [], this.opt);
+        List.renderRow(this.element, this.cols, this.numbering ? [this.element.rows.length + 1] : [], this.opt)
+    }
+    static equals(arr1, arr2, numbering) {
+        if (arr1 && arr2) {
+            const i = numbering ? 1 : 0;
+            const tmp = arr2.slice(i);
+            return arr1.slice(i).every((v, i) => v === tmp[i]);
+        }
     }
     static renderRow(table, cols, data, opt) {
         const tr = dom.element("tr", table);
@@ -84,9 +98,12 @@ class List extends Component {
             dom.text("th", tr, data[c] || "", opt);
         }
     }
-    static renderRows(table, cols, data, opt) {
+    static renderRows(table, cols, data, numbering, opt) {
+        let i = 1;
         for (const row of data || []) {
-            List.renderRow(table, cols, row, opt);
+
+            const cells = numbering ? [i++, row].flat() : row;
+            List.renderRow(table, cols, cells, opt);
         }
     }
     static visibleColumn(table, col, flag) {
